1dd153b2e110c7641f0cc2a75b07f2e225670d09
Jason Jobe <github@jasonjobe.com>
2016-08-21 22:20:59 -0400

    Initial Commit

---
+++ BagOfTricks/BagOfTricks.h
@@ -0,0 +1,19 @@
+//
+//  BagOfTricks.h
+//  BagOfTricks
+//
+//  Created by Jason Jobe on 8/21/16.
+//  Copyright © 2016 WildThink. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BagOfTricks.
+FOUNDATION_EXPORT double BagOfTricksVersionNumber;
+
+//! Project version string for BagOfTricks.
+FOUNDATION_EXPORT const unsigned char BagOfTricksVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BagOfTricks/PublicHeader.h>
+
+
