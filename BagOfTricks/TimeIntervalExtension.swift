790f47cac3ef8da75c4a5a6c1cd5ae3576f01566
Jason Jobe <github@jasonjobe.com>
2016-12-19 22:46:56 -0500

    Working on Playground

--- BagOfTricks/TimeIntervalExtension.swift
+++ BagOfTricks/TimeIntervalExtension.swift
@@ -35,3 +35,32 @@ public extension TimeInterval
     public var in_years: Double   { return self.in_days / 365 }
     
 }
+
+
+public extension Int
+{
+    public var seconds: Double { return Double(self) }
+    public var minutes: Double { return Double(self) * 60 }
+    public var hours: Double   { return Double(self).minutes * 60 }
+    public var days: Double    { return Double(self).hours * 24 }
+    public var weeks: Double   { return Double(self).days * 7 }
+    public var months: Double  { return Double(self).days * 30 }
+    public var years: Double   { return Double(self).days * 365 }
+
+    public var second: Double { return Double(self) }
+    public var minute: Double { return Double(self) * 60 }
+    public var hour: Double   { return Double(self).minutes * 60 }
+    public var day: Double    { return Double(self).hours * 24 }
+    public var week: Double   { return Double(self).days * 7 }
+    public var month: Double  { return Double(self).days * 30 }
+    public var year: Double   { return Double(self).days * 365 }
+
+    public var in_seconds: Double { return Double(self) }
+    public var in_minutes: Double { return Double(self) / 60 }
+    public var in_hours: Double   { return Double(self).in_minutes / 60 }
+    public var in_days: Double    { return Double(self).in_hours / 24 }
+    public var in_weeks: Double   { return Double(self).in_days / 7 }
+    public var in_months: Double  { return Double(self).in_days / 30 }
+    public var in_years: Double   { return Double(self).in_days / 365 }
+    
+}
