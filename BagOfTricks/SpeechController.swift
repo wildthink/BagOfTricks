9d8b831e810a5ea02d5a26e201da3fa78ba18559
Jason Jobe <github@jasonjobe.com>
2017-01-09 19:50:50 -0500

    added simple Speech stuff

---
+++ BagOfTricks/SpeechController.swift
@@ -0,0 +1,105 @@
+//
+//  SpeechController.swift
+//  SpeechRecognition
+//
+//  Created by Stephen Anthony on 24/11/2016.
+//  Copyright © 2016 Darjeeling Apps. All rights reserved.
+//
+
+import Foundation
+import Speech
+
+/// The class used to control speech recognition sessions.
+@available(iOS 10.0, *)
+open class SpeechController {
+    private static let inputNodeBus: AVAudioNodeBus = 0
+    
+    /// The speech recogniser used by the controller to record the user's speech.
+    private let speechRecogniser = SFSpeechRecognizer(locale: Locale(identifier: "en-US"))!
+    
+    /// The current speech recognition request. Created when the user wants to begin speech recognition.
+    private var recognitionRequest: SFSpeechAudioBufferRecognitionRequest?
+    
+    /// The current speech recognition task. Created when the user wants to begin speech recognition.
+    private var recognitionTask: SFSpeechRecognitionTask?
+    
+    /// The audio engine used to record input from the microphone.
+    private let audioEngine = AVAudioEngine()
+    
+    /// The delegate of the receiver.
+    var delegate: SpeechControllerDelegate?
+    
+    /// Begins a new speech recording session.
+    ///
+    /// - Throws: Errors thrown by the creation of the speech recognition session
+    open func startRecording() throws {
+        guard speechRecogniser.isAvailable else {
+            // Speech recognition is unavailable, so do not attempt to start.
+            return
+        }
+        if let recognitionTask = recognitionTask {
+            // We have a recognition task still running, so cancel it before starting a new one.
+            recognitionTask.cancel()
+            self.recognitionTask = nil
+        }
+        
+        guard SFSpeechRecognizer.authorizationStatus() == .authorized else {
+            SFSpeechRecognizer.requestAuthorization({ _ in })
+            return
+        }
+        
+        let audioSession = AVAudioSession.sharedInstance()
+        try audioSession.setCategory(AVAudioSessionCategoryRecord)
+        try audioSession.setMode(AVAudioSessionModeMeasurement)
+        try audioSession.setActive(true, with: .notifyOthersOnDeactivation)
+        
+        recognitionRequest = SFSpeechAudioBufferRecognitionRequest()
+        
+        guard let inputNode = audioEngine.inputNode, let recognitionRequest = recognitionRequest else {
+            throw SpeechControllerError.noAudioInput
+        }
+        
+        recognitionTask = speechRecogniser.recognitionTask(with: recognitionRequest) { [unowned self] result, error in
+            if let result = result {
+                self.delegate?.speechController(self, didRecogniseText: result.bestTranscription.formattedString)
+            }
+            
+            if result?.isFinal ?? (error != nil) {
+                inputNode.removeTap(onBus: SpeechController.inputNodeBus)
+            }
+        }
+        
+        let recordingFormat = inputNode.outputFormat(forBus: SpeechController.inputNodeBus)
+        inputNode.installTap(onBus: SpeechController.inputNodeBus, bufferSize: 1024, format: recordingFormat) { (buffer: AVAudioPCMBuffer, when: AVAudioTime) in
+            self.recognitionRequest?.append(buffer)
+        }
+        
+        audioEngine.prepare()
+        try audioEngine.start()
+    }
+    
+    /// Ends the current speech recording session.
+    open func stopRecording() {
+        audioEngine.stop()
+        recognitionRequest?.endAudio()
+    }
+}
+
+/// The protocol to conform to for delegates of `SpeechController`.
+@available(iOS 10.0, *)
+public protocol SpeechControllerDelegate {
+    
+    /// The message sent when the user's speech has been transcribed. Will be called with partial results of the current recording session.
+    ///
+    /// - Parameters:
+    ///   - speechController: The controller sending the message.
+    ///   - text: The text transcribed from the user's speech.
+    func speechController(_ speechController: SpeechController, didRecogniseText text: String)
+}
+
+/// The error types vended by `SpeechController` if it cannot create an audio recording session.
+///
+/// - noAudioInput: No audio input connection could be created.
+public enum SpeechControllerError: Error {
+    case noAudioInput
+}
---
+++ BagOfTricks/SpeechRecordingControl.swift
@@ -0,0 +1,198 @@
+//
+//  SpeechRecordingControl.swift
+//  SpeechRecognition
+//
+//  Created by Stephen Anthony on 24/11/2016.
+//  Copyright © 2016 Darjeeling Apps. All rights reserved.
+//
+
+import UIKit
+
+/// The control used to allow the user to begin and end speech recording sessions.
+@IBDesignable
+open class SpeechRecordingControl: UIControl {
+    static let preferredDiameter: CGFloat = 60
+    static let expandedDiameter: CGFloat = 80
+    
+    private let microphoneCircleView = CircleView(colour: .blue, diameter: SpeechRecordingControl.preferredDiameter, image: UIImage(named: "Microphone", in: Bundle(for: SpeechRecordingControl.self), compatibleWith: nil)!.withRenderingMode(.alwaysTemplate))
+    private var microphoneCircleViewCentreYConstraint: NSLayoutConstraint?
+    private let greyCircleView = CircleView(colour: UIColor(white: 0.9, alpha: 1.0), diameter: SpeechRecordingControl.preferredDiameter)
+    
+    private var isRecording = false {
+        didSet {
+            microphoneCircleView.diameter = isRecording ? SpeechRecordingControl.expandedDiameter : SpeechRecordingControl.preferredDiameter
+//            microphoneCircleViewCentreYConstraint?.constant = isRecording ? -100 : 0
+        }
+    }
+
+    override public init(frame: CGRect) {
+        super.init(frame: frame)
+        commonSetup()
+    }
+    
+    required public init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonSetup()
+    }
+    
+    override open func didMoveToSuperview() {
+        microphoneCircleView.colour = tintColor
+    }
+    
+    override open func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        isRecording(true, animated: true)
+        sendActions(for: .touchDown)
+    }
+    
+    override open func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else {
+            return
+        }
+        
+        let controlEvent: UIControlEvents = bounds.contains(touch.location(in: self)) ? .touchUpInside : .touchUpOutside
+        isRecording(false, animated: true)
+        sendActions(for: controlEvent)
+    }
+    
+    private func commonSetup() {
+        // Configure our size.
+        if #available(iOS 9.0, *) {
+            NSLayoutConstraint.activate([
+                widthAnchor.constraint(equalToConstant: SpeechRecordingControl.preferredDiameter),
+                heightAnchor.constraint(equalToConstant: SpeechRecordingControl.preferredDiameter)
+                ])
+        } else {
+            // Fallback on earlier versions
+        }
+        
+        greyCircleView.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(greyCircleView)
+        if #available(iOS 9.0, *) {
+            NSLayoutConstraint.activate([
+                greyCircleView.centerXAnchor.constraint(equalTo: centerXAnchor),
+                greyCircleView.centerYAnchor.constraint(equalTo: centerYAnchor)
+                ])
+        } else {
+            // Fallback on earlier versions
+        }
+        
+        microphoneCircleView.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(microphoneCircleView)
+        if #available(iOS 9.0, *) {
+            microphoneCircleViewCentreYConstraint = microphoneCircleView.centerYAnchor.constraint(equalTo: centerYAnchor)
+        } else {
+            // Fallback on earlier versions
+        }
+        if #available(iOS 9.0, *) {
+            NSLayoutConstraint.activate([
+                microphoneCircleView.centerXAnchor.constraint(equalTo: centerXAnchor),
+                microphoneCircleViewCentreYConstraint!
+                ])
+        } else {
+            // Fallback on earlier versions
+        }
+    }
+    
+    private func isRecording(_ isRecording: Bool, animated: Bool) {
+        self.isRecording = isRecording
+        microphoneCircleView.isImageAnimating = isRecording
+        if animated {
+            UIView.animate(withDuration: 0.5, delay: 0.0, usingSpringWithDamping: isRecording ? 0.8 : 1.0, initialSpringVelocity: isRecording ? 20 : 0, options: .curveLinear, animations: {
+                self.layoutIfNeeded()
+            }, completion: nil)
+        }
+    }
+}
+
+/// The view subclass used to draw circles.
+fileprivate class CircleView: UIView {
+    private var widthConstraint: NSLayoutConstraint!
+    private lazy var imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .center
+        imageView.tintColor = .white
+        return imageView
+    }()
+    
+    init(colour: UIColor, diameter: CGFloat, image: UIImage? = nil) {
+        super.init(frame: CGRect.zero)
+        self.colour = colour
+        self.diameter = diameter
+        if #available(iOS 9.0, *) {
+            widthConstraint = widthAnchor.constraint(equalToConstant: diameter)
+        } else {
+            // Fallback on earlier versions
+        }
+        isOpaque = false
+        isUserInteractionEnabled = false
+        imageView.image = image
+        addSubview(imageView)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError()
+    }
+    
+    /// The diameter of the receiver.
+    var diameter: CGFloat = 0 {
+        didSet {
+            widthConstraint.constant = diameter
+        }
+    }
+    
+    /// The colour of the receiver.
+    var colour = UIColor.white {
+        didSet {
+            setNeedsDisplay()
+        }
+    }
+    
+    /// Controls whether the image view animates to highlight its contents or not.
+    var isImageAnimating = false {
+        didSet {
+            guard isImageAnimating else {
+                imageView.layer.removeAllAnimations()
+                return
+            }
+            let transformAnimation = CABasicAnimation(keyPath: "transform.scale")
+            transformAnimation.duration = 0.5
+            transformAnimation.fromValue = 1.0
+            transformAnimation.toValue = 1.2
+            transformAnimation.repeatCount = .infinity
+            transformAnimation.autoreverses = true
+            transformAnimation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)
+            let opacityAnimation = CABasicAnimation(keyPath: "opacity")
+            opacityAnimation.duration = transformAnimation.duration
+            opacityAnimation.fromValue = 0.7
+            opacityAnimation.toValue = 1.0
+            opacityAnimation.repeatCount = transformAnimation.repeatCount
+            opacityAnimation.autoreverses = transformAnimation.autoreverses
+            opacityAnimation.timingFunction = transformAnimation.timingFunction
+            imageView.layer.add(transformAnimation, forKey: nil)
+            imageView.layer.add(opacityAnimation, forKey: nil)
+        }
+    }
+    
+    fileprivate override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        colour.set()
+        UIBezierPath(ovalIn: rect).fill()
+    }
+    
+    fileprivate override func updateConstraints() {
+        if #available(iOS 9.0, *) {
+            NSLayoutConstraint.activate([
+                widthConstraint,
+                heightAnchor.constraint(equalTo: widthAnchor),
+                imageView.leadingAnchor.constraint(equalTo: leadingAnchor),
+                imageView.trailingAnchor.constraint(equalTo: trailingAnchor),
+                imageView.topAnchor.constraint(equalTo: topAnchor),
+                imageView.bottomAnchor.constraint(equalTo: bottomAnchor)
+                ])
+        } else {
+            // Fallback on earlier versions
+        }
+        super.updateConstraints()
+    }
+}
